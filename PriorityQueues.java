064c0baf8a501f3af85fb67376566b70e25ff7cf
Arif <arifonly4@gmail.com>
2023-01-23 23:57:49 +0530

    Heaps

--- PriorityQueues.java
+++ PriorityQueues.java
@@ -1,8 +1,10 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueues{
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
         pq.add(3); // O(logn)
         pq.add(4);
