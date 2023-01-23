079be18ac46018c18d6ba6cad715d73ce2387abb
Arif <arifonly4@gmail.com>
2023-01-23 23:52:21 +0530

    Heaps

---
+++ PriorityQueues.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueues{
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(3); // O(logn)
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while(!pq.isEmpty()) {
+            System.out.println(pq.peek()); // O(1)
+            pq.remove(); // O(logn)
+        }
+    }
+}
